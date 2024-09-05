aadbaf1c5f0610944f668a96b747f31352bb2dc0
Dennis Muoki <muokid3@gmail.com>
2024-09-05 10:06:00 +0300

    added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id ("kotlin-kapt")
+    id ("com.google.dagger.hilt.android")
 }
 
 android {
@@ -66,4 +68,49 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+
+    //Splash Api
+    implementation ("androidx.core:core-splashscreen:1.0.1")
+
+    //Compose Navigation
+    val nav_version = "2.7.7"
+    implementation ("androidx.navigation:navigation-compose:$nav_version")
+
+    //Dagger Hilt
+    implementation ("com.google.dagger:hilt-android:2.52")
+    kapt ("com.google.dagger:hilt-compiler:2.52")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
+    // For instrumentation tests
+    androidTestImplementation  ("com.google.dagger:hilt-android-testing:2.52")
+    androidTestAnnotationProcessor ("com.google.dagger:hilt-compiler:2.52")
+    // For local unit tests
+    testImplementation ("com.google.dagger:hilt-android-testing:2.52")
+    testAnnotationProcessor ("com.google.dagger:hilt-compiler:2.52")
+
+    //Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    //logging interceptor - for logging
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
+    //Coil
+    implementation("io.coil-kt:coil-compose:2.4.0")
+
+    //Datastore
+    implementation ("androidx.datastore:datastore-preferences:1.1.1")
+
+    //Room
+    val room_version = "2.6.1"
+    implementation ("androidx.room:room-runtime:$room_version")
+    kapt ("androidx.room:room-compiler:$room_version")
+    implementation ("androidx.room:room-ktx:$room_version")
+
+    //material Icons extended
+    implementation("androidx.compose.material:material-icons-extended:1.7.0-beta07")
+}
+
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
 }
